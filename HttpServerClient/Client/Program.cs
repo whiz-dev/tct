224f928c2d337a82534773e3a3e116fc746118e7
whiz-dev <whiz123@hanmail.net>
2022-06-11 18:59:37 +0900

    initial commit

---
+++ HttpServerClient/Client/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpClient client = new HttpClient();
+
+            Console.WriteLine("Main Thread : " + Thread.CurrentThread.ManagedThreadId);
+
+            while (true)
+            {
+                string msg = Console.ReadLine();
+
+                if (msg.ToUpper() == "Q")
+                {
+                    break;
+                }
+
+                if (msg.ToUpper() == "N")
+                {
+                    GetRequestAync_ThreadNonblock(client);
+                }
+                else
+                {
+                    GetRequestAsync_ThreadBlock(client);
+                    //PostRequestAsync_ThreadBlock(client);
+                }
+            }
+        }
+
+        /// <summary>
+        /// GetAsync & Thread Nonblock
+        /// </summary>
+        /// <param name="client"></param>
+        static async void GetRequestAync_ThreadNonblock(HttpClient client)
+        {
+            // Before Thread = Main Thread
+            Console.WriteLine("Before Thread : " + Thread.CurrentThread.ManagedThreadId);
+            
+            HttpResponseMessage res = await client.GetAsync("http://127.0.0.1:8080/helloworld?name=park&age=40");
+
+            // After Thread = Worker Thread
+            Console.WriteLine("After Thread : " + Thread.CurrentThread.ManagedThreadId);
+
+            Console.WriteLine("Response : " + res.StatusCode);
+            Console.WriteLine("Response : " + await res.Content.ReadAsStringAsync());
+        }
+
+        /// <summary>
+        /// GetAsync & Thread Block
+        /// </summary>
+        /// <param name="client"></param>
+        /// <returns></returns>
+        static void GetRequestAsync_ThreadBlock(HttpClient client)
+        {
+            Task<HttpResponseMessage> tsk = client.GetAsync("http://127.0.0.1:8080/helloworld?name=park&age=40");
+            tsk.Wait();
+
+            HttpResponseMessage res = tsk.Result;
+
+            Console.WriteLine("Response : " + res.StatusCode);
+            Console.WriteLine("Response : " + res.Content.ReadAsStringAsync().Result);
+        }
+
+        static void PostRequestAsync_ThreadBlock(HttpClient client)
+        {
+            StringContent body = new StringContent("안녕!", Encoding.UTF8);
+            Task<HttpResponseMessage> tsk = client.PostAsync("http://127.0.0.1:8080/helloworld", body);
+            tsk.Wait();
+
+            HttpResponseMessage res = tsk.Result;
+
+            Console.WriteLine("Response : " + res.StatusCode);
+            Console.WriteLine("Response : " + res.Content.ReadAsStringAsync().Result);
+        }
+    }
+}
---
+++ HttpServerClient/Server/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpListener server = new HttpListener();
+            server.Prefixes.Add("http://127.0.0.1:8080/");
+            server.Start();
+            Console.WriteLine("Server Started");
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+            CancellationToken token = cts.Token;
+            Task tsk = Task.Factory.StartNew(() => { AcceptRequest(server, token); });
+
+            Console.ReadLine();
+
+            cts.Cancel();
+            tsk.Wait();
+            Console.WriteLine("Cancel Accept Request");
+
+            server.Close();
+            Console.WriteLine("Server Closed");
+
+            Console.ReadLine();
+        }
+
+        static void AcceptRequest(HttpListener server, CancellationToken token)
+        {
+            while (true)
+            {
+                Task<HttpListenerContext> tsk = server.GetContextAsync();
+
+                try
+                {
+                    tsk.Wait(token);
+
+                    Task.Factory.StartNew(() => { ProcessRequest(tsk.Result); });
+
+                    //Thread thr = new Thread(new ParameterizedThreadStart(ProcessRequest));
+                    //thr.Start(tsk.Result);
+                }
+                catch
+                {
+                    break;
+                }
+            }
+        }
+
+        static void ProcessRequest(object result)
+        {
+            HttpListenerContext context = (HttpListenerContext)result;
+            HttpListenerRequest request = (HttpListenerRequest)context.Request;
+            HttpListenerResponse response = (HttpListenerResponse)context.Response;
+
+            // Request Info
+            Console.WriteLine("### Request Info ###");
+            Console.WriteLine("Url : " + request.Url);
+            Console.WriteLine("RawUrl : " + request.RawUrl);
+            Console.WriteLine("HttpMethod : " + request.HttpMethod);
+            Console.WriteLine("ContentType : " + request.ContentType);
+            Console.WriteLine("ContentEncoding : " + request.ContentEncoding.EncodingName);
+            Console.WriteLine("ContentLength : " + request.ContentLength64);
+
+            foreach (string key in context.Request.QueryString.AllKeys)
+            {
+                Console.WriteLine("QueryString : " + key + ", " + context.Request.QueryString[key]);
+            }
+
+            System.IO.StreamReader reader = new System.IO.StreamReader(request.InputStream, request.ContentEncoding);
+            string body = reader.ReadToEnd();
+            reader.Close();
+            Console.WriteLine("Body : " + body);
+
+            // Do some work for 5 Secondsa
+            Thread.Sleep(5000);
+            byte[] data = Encoding.UTF8.GetBytes("안녕!");
+
+            // Response
+            response.ContentEncoding = Encoding.UTF8;
+            response.StatusCode = 200;
+            response.OutputStream.Write(data, 0, data.Length);
+            response.Close();
+        }
+    }
+}
