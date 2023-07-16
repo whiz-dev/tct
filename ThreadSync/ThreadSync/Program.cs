42470809ef55f9499a96c4f093d5e0ef701de572
whiz-dev <whiz123@hanmail.net>
2023-07-16 23:36:47 +0900

    Thread 동기화 예제

---
+++ ThreadSync/ThreadSync/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadSync
+{
+    internal class Program
+    {
+        static ConcurrentDictionary<string, SyncManager> _syncManagers = new ConcurrentDictionary<string, SyncManager>();
+
+        static void Main(string[] args)
+        {
+            List<string> reqCmds = new List<string>();
+            reqCmds.Add("A");
+            reqCmds.Add("A");
+            reqCmds.Add("B");
+            reqCmds.Add("C");
+            reqCmds.Add("A");
+
+            List<ServerProcess> svrs = new List<ServerProcess>();
+            svrs.Add(new ServerProcess("SVR1"));
+            svrs.Add(new ServerProcess("SVR2"));
+
+            foreach (string cmd in reqCmds)
+            {
+                foreach (ServerProcess svr in svrs)
+                {
+                    Thread thr = new Thread(new ParameterizedThreadStart(Request));
+                    thr.Start(new object[] { svr, cmd });
+                }
+            }
+        }
+
+        static void Request(object obj)
+        {
+            ServerProcess svr = ((object[])obj)[0] as ServerProcess;
+            string cmd = ((object[])obj)[1] as string;
+
+            _syncManagers.TryAdd(svr.ServerName, new SyncManager(3));
+            _syncManagers[svr.ServerName].SetStart(cmd);
+
+            try
+            {
+                Console.WriteLine($"[{DateTime.Now.ToString()}] [{svr.ServerName}] [{cmd}] Request Start!");
+                svr.ExecuteCommand(cmd);
+                Console.WriteLine($"[{DateTime.Now.ToString()}] [{svr.ServerName}] [{cmd}] Request Completed!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                _syncManagers[svr.ServerName].SetEnd(cmd);
+            }
+        }
+    }
+}
---
+++ ThreadSync/ThreadSync/SyncManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadSync
+{
+    public class SyncManager
+    {
+        private object _objStart = new object();
+
+        private SemaphoreSlim _countSync = null;
+        private ConcurrentDictionary<string, SemaphoreSlim> _cmdSync = new ConcurrentDictionary<string, SemaphoreSlim>();
+
+        public SyncManager(int maxCount)
+        {
+            _countSync = new SemaphoreSlim(maxCount, maxCount);
+        }
+
+        public void SetStart(string cmd)
+        {
+            // 입력된 cmd 순서대로 순차처리 필요시 lock
+            lock (_objStart)
+            {
+                _cmdSync.TryAdd(cmd, new SemaphoreSlim(1, 1));
+
+                _cmdSync[cmd].Wait();
+                _countSync.Wait();
+            }
+        }
+
+        public void SetEnd(string cmd)
+        {
+            _cmdSync[cmd].Release();
+            _countSync.Release();
+        }
+    }
+}
